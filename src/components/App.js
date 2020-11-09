73d79216fefa07b415cc1ff318ac5f05a22d9ba6
wissmedia <wissmedia@gmail.com>
2020-11-09 17:10:03 +0800

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import theme from './ui/Theme'
 import Header from '../components/ui/Header'
+import Footer from './ui/Footer'
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [value, setValue] = useState(0)
+  
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Header />
+        <Header value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
         <Switch>
           <Route exact path="/" component={() => <div>Home</div>} />
           <Route exact path="/services" component={() => <div>Services</div>} />
@@ -19,11 +23,11 @@ function App() {
           <Route exact path="/revolution" component={() => <div>The Revolution</div>} />
           <Route exact path="/about" component={() => <div>About Us</div>} />
           <Route exact path="/contact" component={() => <div>Contact Us</div>} />
-          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+          <Route exact path="/estimate" component={() => <div>Free Estimate</div>} />
         </Switch>
+        <Footer value={value} setValue={setValue} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex} />
       </BrowserRouter>
     </ThemeProvider>
   );
 }
-
 export default App;
